d59d731fef361b222aa6489a8413d6268f870900
dmoldabayev <dmoldabayev.99@gmail.com>
2021-12-01 15:11:12 +0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExploreComponent } from './components/pages/explore/explore.component';
+import { MyRoomsComonent } from './components/pages/my-rooms/my-rooms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ExploreComponent},
+  { path: 'explore', component: ExploreComponent},
+  { path: 'my-rooms', component: MyRoomsComonent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
---
+++ src/app/components/pages/my-rooms/my-rooms.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-rooms',
+  templateUrl: 'my-rooms.component.html'
+})
+export class MyRoomsComonent {
+}
