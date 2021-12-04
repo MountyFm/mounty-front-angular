db995f80bd1c1802703fcae62eabd6cb98d12947
dmoldabayev <dmoldabayev.99@gmail.com>
2021-12-04 17:40:31 +0600

    added login button + logic to redirect to spotify auth page

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -5,17 +5,20 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 @Component({
   selector: 'login',
   templateUrl: 'login.component.html',
+  styleUrls: ['./login.component.css'],
   providers: [AuthService],
 })
 export class LoginComponent implements OnInit {
 
   constructor(private authService: AuthService) {}
 
+  public authUrl!: string
+
   ngOnInit(): void {
     this.authorize();
   }
 
   authorize(): void {
-    this.authService.getAuthUrl().subscribe(response => window.location.href = response.url);
+    this.authService.getAuthUrl().subscribe(response => this.authUrl = response.url);
   }
 }
